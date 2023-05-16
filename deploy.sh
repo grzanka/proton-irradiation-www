c256bea6370e6f5aa4df64aa40820fbd31d6211e
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-05-16 15:02:24 +0200

    a

--- deploy.sh
+++ deploy.sh
@@ -53,22 +53,23 @@ then
 else
     echo "Uploading site to server from $PWD/site to $LFTP_PATH on $LFTP_HOST:$LFTP_PORT as $LFTP_USER"
 
-    # # list of files and directories on server
-    # lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "cls $LFTP_PATH; quit" > server_contents.txt
+    # list of files and directories on server
+    lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "cls $LFTP_PATH; quit" > server_contents.txt
 
-    # # remove all files and directories on server contained in local server_contents.txt file
-    # while read -r line; do
-    #     if [[ $line == *"$LFTP_PATH"* ]]; then
-    #         lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "rm -rf $line; quit"
-    #     fi
-    # done < server_contents.txt
+    # remove all files and directories on server contained in local server_contents.txt file
+    while read -r line; do
+        if [[ $line == *"$LFTP_PATH"* ]]; then
+            lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "rm -rf $line; quit"
+        fi
+    done < server_contents.txt
 
-    # lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "cls -al $LFTP_PATH; quit"
+    # check if all the files were removed
+    lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "cls -al $LFTP_PATH; quit"
 
     # find all directories and files in site/ and upload them to server
     #find site/ -type d -execdir lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "rm -rf $LFTP_PATH{}; mkdir $LFTP_PATH{}; chmod o+rx $LFTP_PATH{}; quit" \;
     #find site/ -type d -execdir lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "rmdir $LFTP_PATH{}; chmod o+rx $LFTP_PATH{}; quit" \;
-    lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "mirror --delete --reverse site/ $LFTP_PATH; chmod --recursive o-w $LFTP_PATH; chmod --recursive o+r $LFTP_PATH; cls -al $LFTP_PATH; quit"
+    #lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "mirror --delete --reverse site/ $LFTP_PATH; chmod --recursive o-w $LFTP_PATH; chmod --recursive o+r $LFTP_PATH; cls -al $LFTP_PATH; quit"
 fi
 
 # Deactivate virtual environment
